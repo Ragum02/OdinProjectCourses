722dbbb601a0581a1f7be837425265c4f07084f4
Ragum02 <benjamin.souiket@gmail.com>
2024-10-10 09:44:11 +0200

    let / const difference

--- odin-jvscript-firsttry/javascript.js
+++ odin-jvscript-firsttry/javascript.js
@@ -1,5 +1,15 @@
 let name = "John";
 let surname = "Doe";
+let age = "18"
 
+console.log(age)
+
+age = "29";
+
+console.log(age);
 console.log(name);
 console.log(surname);
+
+const pi = 3.14;
+pi = 10;
+console.log(pi); // (error because we can't reassign a const) //
